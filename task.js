8c623cfa112f5a028e2159f564dd392cae1389e6
ROKSANA DILSHAD <lagonlegend847@gmail.com>
2025-08-02 21:21:05 +0600

    modify

--- task.js
+++ task.js
@@ -1,12 +1,13 @@
-let moneyGiven = 100;
+let moneyGiven = 1000;
 
-let orangePrice = 10;
-let applePrice = 20;
+let orangePrice = 100;
+let applePrice = 200;
 
 let orangeQty = 3;
 let appleQty = 2;
 
 let totalCost = (orangePrice * orangeQty) + (applePrice * appleQty);
+let takenMoney = (moneyGiven-totalCost)
  
-console.log("Money returned by shopkeeper:", totalCost + "taka");
+console.log("Money returned by shopkeeper:", takenMoney + "taka");
 
